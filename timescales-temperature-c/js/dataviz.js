4b8e6772eb10744739e4b617643e2c66b6dec897
Brian Foo <brian@youaremyjoy.org>
2017-08-01 18:12:36 -0400

    timescales c: add transitions #25

--- timescales-temperature-c/js/dataviz.js
+++ timescales-temperature-c/js/dataviz.js
@@ -7,6 +7,7 @@ var DataViz = (function() {
       margin: [0.1, 0.15, 0.1, 0.1],
       enableSound: true,
       transitionPlotMs: 1000,
+      highlightMs: 200,
       minDomainCount: 5,
       yAxisStep: 0.1,
       axisTextStyle: {
@@ -48,8 +49,8 @@ var DataViz = (function() {
 
     // init plot
     this.plotData = [];
-    this.plotDomain = this.domain.slice(0);
-    this.plotRange = this.range.slice(0);
+    this.plotDomain = [];
+    this.plotRange = [];
     this.plotIndex = 0;
     this.plotYear = {};
 
@@ -71,10 +72,11 @@ var DataViz = (function() {
     this.app = new PIXI.Application(this.$el.width(), this.$el.height(), {transparent : true});
     this.axes = new PIXI.Graphics();
     this.plot = new PIXI.Graphics();
+    this.highlight = new PIXI.Graphics();
     this.labels = new PIXI.Graphics();
     this.marker = new PIXI.Graphics();
 
-    this.app.stage.addChild(this.plot, this.axes, this.labels, this.marker);
+    this.app.stage.addChild(this.plot, this.highlight, this.axes, this.labels, this.marker);
 
     this.$el.append(this.app.view);
   };
@@ -87,10 +89,10 @@ var DataViz = (function() {
     this.renderMarker();
   };
 
-  DataViz.prototype.render = function(progress){
-    if (!this.plotData || !this.domain || !this.range) return false;
+  DataViz.prototype.render = function(){
 
 
+    this.transition();
 
 
   };
@@ -173,6 +175,52 @@ var DataViz = (function() {
     }
   };
 
+  DataViz.prototype.renderHighlight = function(){
+    var _this = this;
+    var data = this.plotData;
+    var domain = this.plotDomain;
+    var range = this.plotRange;
+    var w = this.app.renderer.width;
+    var h = this.app.renderer.height;
+    var m = this.opt.margin;
+    var mx0 = m[0] * w;
+    var my0 = m[1] * h;
+    var mx1 = m[2] * w;
+    var my1 = m[3] * h;
+
+    this.highlight.clear();
+    // while(this.plot.children[0]) {
+    //   this.plot.removeChild(this.plot.children[0]);
+    // }
+
+    var cw = w - mx0 - mx1;
+    var ch = h - my0 - my1;
+    var dataW = cw / data.length;
+    var dataMargin = 0.5;
+    _.each(data, function(d, i){
+      if (d.highlighting) {
+        var hv = d.hightlightValue;
+        var p = _this._dataToPoint(d.year, d.value, domain, range);
+        var px = UTIL.norm(d.year, domain[0], domain[1]+1);
+        var x = i * dataW + mx0 + dataMargin;
+        var y = p[1];
+        var pw = dataW-dataMargin*2;
+        var ph = h-y-my1;
+        var scaleY = 1 + hv/2;
+        var scaleX = 1 + hv;
+        var diffW = pw*scaleX - pw;
+        var diffH = ph*scaleY - ph;
+        pw *= scaleX;
+        ph *= scaleY;
+        x -= diffW/2;
+        y -= diffH;
+        _this.plot.beginFill(0xFFFFFF, (1.0-hv) * 0.5);
+        _this.plot.drawRect(x, y, pw, ph);
+      }
+
+    });
+  };
+
   DataViz.prototype.renderLabels = function(){
     // this.labels.clear();
     // while(this.labels.children[0]) {
@@ -248,7 +296,9 @@ var DataViz = (function() {
     var dataW = cw / data.length;
     var dataMargin = 0.5;
     _.each(data, function(d, i){
-      var p = _this._dataToPoint(d.year, d.value, domain, range);
+      var value = d.value;
+      if (d.transitioning) value = d.transitionValue;
+      var p = _this._dataToPoint(d.year, value, domain, range);
       var px = UTIL.norm(d.year, domain[0], domain[1]+1);
       var x = i * dataW + mx0 + dataMargin;
       var y = p[1];
@@ -258,6 +308,47 @@ var DataViz = (function() {
     });
   };
 
+  DataViz.prototype.transition = function(){
+    if (!this.transitioning) return false;
+
+    var _this = this;
+    var range = this.plotRange;
+    var transitionPlotMs = this.opt.transitionPlotMs;
+    var highlightMs = this.opt.highlightMs;
+    var transitioning = false;
+    var now = new Date();
+
+    _.each(this.plotData, function(d, i){
+      if (d.transitioning && d.transitionStart) {
+        var diff = now - d.transitionStart;
+        if (diff >= transitionPlotMs) {
+          diff = transitionPlotMs;
+          _this.plotData[i].transitioning = false;
+        } else {
+          transitioning = true;
+        }
+        var progress = diff / transitionPlotMs;
+        progress = UTIL.easeInElastic(progress, 0.005);
+        _this.plotData[i].transitionValue = UTIL.lerp(range[0], d.value, progress);
+      }
+      if (d.highlighting && d.highlightStart) {
+        var diff = now - d.highlightStart;
+        if (diff >= highlightMs) {
+          diff = highlightMs;
+          _this.plotData[i].highlighting = false;
+        } else {
+          transitioning = true;
+        }
+        var progress = diff / transitionPlotMs;
+        _this.plotData[i].hightlightValue = progress;
+      }
+    });
+
+    this.transitioning = transitioning;
+    this.renderPlot();
+    this.renderHighlight();
+  };
+
   DataViz.prototype.update = function(){
     var _this = this;
 
@@ -265,37 +356,78 @@ var DataViz = (function() {
     var maxDomainCount = this.dataLen;
     var domainCount = Math.round(UTIL.lerp(minDomainCount, maxDomainCount, this.scale));
 
+    // add years to the left and right of current marker
     var add = Math.round(domainCount/2);
     var minIndex = this.plotIndex - add;
     var maxIndex = this.plotIndex + add;
 
+    // adjust edges
     if (minIndex < 0) {
       maxIndex += (minIndex*-1);
       minIndex = 0;
     }
-
     if (maxIndex >= maxDomainCount) {
       minIndex -= (maxIndex-maxDomainCount+1)
       maxIndex = maxDomainCount - 1;
     }
-
     minIndex = Math.max(minIndex, 0);
     maxIndex = Math.min(maxIndex, maxDomainCount - 1);
 
+    var prevDomain = this.plotDomain.slice(0);
     var d0 = this.domain[0];
     var domain = [d0+minIndex, d0+maxIndex];
     this.plotDomain = domain;
 
-    this.plotData = _.filter(this.data, function(d, i){ return i >= minIndex && i<= maxIndex; });
-    var values = _.pluck(this.plotData, "value");
+    // determine which indices to add
+    var addIndices = [];
+    if (prevDomain.length && domain[0] < prevDomain[0]) {
+      _(prevDomain[0]-domain[0]).times(function(i){
+        addIndices.push(domain[0] + i - d0);
+      });
+    }
+    if (prevDomain.length && domain[1] > prevDomain[1]) {
+      _(domain[1]-prevDomain[1]).times(function(i){
+        addIndices.push(domain[1] - i - d0);
+      });
+    }
+    if (!prevDomain.length) {
+      _(domain[1]-domain[0]+1).times(function(i){
+        addIndices.push(domain[0] + i - d0);
+      });
+    }
 
+    // remove anything not in index
+    var plotData = _.filter(this.plotData, function(d, i){
+      return (d.index >= minIndex && d.index <= maxIndex);
+    });
+
+    // add new indices
+    _.each(addIndices, function(index, i){
+      var add = _.clone(_this.data[index]);
+      add.transitionStart = new Date();
+      add.transitioning = true;
+      add.transitionValue = 0;
+      plotData.push(add);
+    });
+    plotData = _.sortBy(plotData, 'index');
+    this.plotData = plotData;
+
+    // add transition for index
+    var i = this.plotIndex - minIndex;
+    this.plotData[i].highlighting = true;
+    this.plotData[i].highlightStart = new Date();
+    this.plotData[i].hightlightValue = 0;
+
+    var values = _.pluck(plotData, "value");
     var yAxisStep = this.opt.yAxisStep;
     var minRange = UTIL.floorToNearest(_.min(values), yAxisStep);
     var maxRange = UTIL.ceilToNearest(_.max(values), yAxisStep);
     this.plotRange = [minRange, maxRange];
 
+    this.transitioning = true;
+
     this.renderAxes();
-    this.renderPlot();
+    this.transition();
     this.renderLabels();
     this.renderMarker();
   };
