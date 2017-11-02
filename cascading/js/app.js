8e411cf046931326e7fa0ed13a94d2badcfcba27
Brian Foo <brian@youaremyjoy.org>
2017-11-02 14:01:11 -0400

    cascading interaction

---
+++ cascading/js/app.js
@@ -0,0 +1,123 @@
+'use strict';
+
+var App = (function() {
+  function App(options) {
+    var defaults = {
+    };
+    this.opt = $.extend({}, defaults, options);
+    this.init();
+  }
+
+  App.prototype.init = function(){
+    var _this = this;
+
+    this.branches = this.opt.branches;
+    this.branchCount = this.branches.length;
+    this.index = 0;
+    this.branch = this.branches[this.index];
+    this.rotation = 0;
+    this.timeouts = [];
+
+    $('#graph > g').each(function(){
+      var $el = $(this);
+      var id = $el.attr('id');
+
+      if (id.length <= 2) {
+        $el.addClass('strong');
+      } else if (id[3] == '2' || id[3] == '3') {
+        $el.addClass('weak');
+      }
+    })
+
+    var nodes = this.branch.nodes;
+    _.each(nodes, function(node, i){
+      $('#'+node).addClass('active');
+    });
+
+    this.loadListeners();
+  };
+
+  App.prototype.loadListeners = function(){
+    var _this = this;
+
+    $(window).on('resize', function(e){
+      _this.onResize();
+    });
+
+    $(document).on("click", function(e) {
+      e.preventDefault();
+      _this.nextBranch(1);
+    });
+
+    $(document).keydown(function(e) {
+      switch(e.which) {
+        case 37: // left
+          e.preventDefault();
+          _this.nextBranch(-1);
+          break;
+
+        case 39: // right
+          e.preventDefault();
+          _this.nextBranch(1);
+          break;
+
+        default: return; // exit this handler for other keys
+      }
+    });
+  };
+
+  App.prototype.nextBranch = function(direction){
+    var _this = this;
+
+    var index = this.index;
+    index += direction;
+
+    if (index < 0) index = this.branchCount - 1;
+    if (index >= this.branchCount) index = 0;
+
+    var currBranch = this.branch;
+    var nextBranch = this.branches[index];
+
+    var rotation = this.rotation;
+    if (direction > 0) rotation += nextBranch.rotate;
+    else rotation -= currBranch.rotate;
+
+    var nodes = nextBranch.nodes;
+
+    $('#graph > g').removeClass('active');
+    $('#svg-container').css('transform', 'rotate('+rotation+'deg)');
+    $('#center').css('transform', 'rotate('+(-rotation)+'deg)');
+
+    _.each(this.timeouts, function(timeout, i){
+      clearTimeout(timeout);
+    });
+
+    var timeouts = [];
+
+    _.each(nodes, function(node, i){
+      var s = (i+1) * 500;
+      var timeout = setTimeout(function(){
+        $('#'+node).addClass('active');
+      }, s);
+      timeouts.push(timeout);
+    });
+
+    this.timeouts = timeouts;
+    this.branch = nextBranch;
+    this.rotation = rotation;
+    this.index = index;
+  };
+
+  App.prototype.onResize = function(){
+    var _this = this;
+
+
+  };
+
+  return App;
+
+})();
+
+$(function() {
+  var app = new App(CONFIG);
+});
---
+++ cascading/js/config.js
@@ -0,0 +1,13 @@
+var CONFIG = {
+  "branches": [
+    {"rotate": 45, "nodes": ["a1", "a2", "a3a1"]},
+    {"rotate": 25, "nodes": ["a1", "a2", "a3b1", "a3b2"]},
+    {"rotate": 70, "nodes": ["b1", "b2", "b3", "b3a1"]},
+    {"rotate": 30, "nodes": ["b1", "b2", "b3", "b3b1", "b3b2"]},
+    {"rotate": 40, "nodes": ["c1", "c1a1", "c1a2"]},
+    {"rotate": 40, "nodes": ["c1", "c1b1", "c1b2", "c1b3"]},
+    {"rotate": 5,  "nodes": ["c1", "c1b1", "c1c1"]},
+    {"rotate": 75, "nodes": ["d1", "d1a1", "d1a2"]},
+    {"rotate": 30, "nodes": ["d1", "d1b1", "d1b2"]}
+  ]
+};
